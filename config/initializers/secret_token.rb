8163ca08d0dd9d4ee28546e3456814a32e70e616
ky004689 <ky004689@etu.unice.fr>
2012-12-14 10:30:19 +0100

    Debut du Projet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projet::Application.config.secret_token = 'ea9b5ad606dd59295f218a68c6cac3c8d2a601d3f2fb9f2719c04ec7b8edd6dc534c106486f3da58a35bfb41370cf5acb9d7c69f0ed6f6347a90ac96906a4743'
